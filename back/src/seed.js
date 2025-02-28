f1d7a4d661e74e255991a591bb77444bd9938efc
aissatou00 <aissatousalla2709@gmail.com>
2025-02-28 16:21:48 +0100

    perf : automatisation de la creation des donnée

--- back/src/seed.js
+++ back/src/seed.js
@@ -3,101 +3,69 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function seed() {
-
-    const user1 = await prisma.user.create({
-        data: {
-            username: 'johndoe',
-            password: 'securepassword1',
-        },
+  
+    const users = await prisma.user.createMany({
+        data: [
+            { username: 'johndoe', password: 'securepassword1' },
+            { username: 'janedoe', password: 'securepassword2' },
+            { username: 'alice', password: 'securepassword3' },
+            { username: 'bob', password: 'securepassword4' },
+            { username: 'charlie', password: 'securepassword5' },
+        ],
+        skipDuplicates: true,
     });
 
-    const user2 = await prisma.user.create({
-        data: {
-            username: 'janedoe',
-            password: 'securepassword2',
-        },
+    console.log('Utilisateurs créés');
+
+  
+    const userList = await prisma.user.findMany();
+
+ 
+    const articles = await prisma.article.createMany({
+        data: [
+            { title: 'Mon premier article', content: 'Contenu du premier article', authorId: userList[0].id },
+            { title: 'Article sur Prisma', content: 'Découverte de Prisma', authorId: userList[1].id },
+            { title: 'JavaScript avancé', content: 'Comprendre les closures', authorId: userList[2].id },
+            { title: 'Les bases de GraphQL', content: 'Pourquoi utiliser GraphQL ?', authorId: userList[3].id },
+            { title: 'TypeScript pour les nuls', content: 'Introduction à TypeScript', authorId: userList[4].id },
+        ],
     });
 
-    console.log('Utilisateurs créés:', user1, user2);
+    console.log('Articles créés');
 
+    const articleList = await prisma.article.findMany();
 
-    const article1 = await prisma.article.create({
-        data: {
-            title: 'Mon premier article',
-            content: 'Ceci est le contenu du premier article.',
-            author: {
-                connect: { id: user1.id },
-            },
-        },
+    const comments = await prisma.comment.createMany({
+        data: [
+            { content: 'Super article !', authorId: userList[1].id, articleId: articleList[0].id },
+            { content: 'Merci pour cet article', authorId: userList[0].id, articleId: articleList[1].id },
+            { content: 'Très bien expliqué', authorId: userList[3].id, articleId: articleList[2].id },
+            { content: 'Intéressant !', authorId: userList[4].id, articleId: articleList[3].id },
+            { content: 'J’adore ce sujet', authorId: userList[2].id, articleId: articleList[4].id },
+        ],
     });
 
-    const article2 = await prisma.article.create({
-        data: {
-            title: 'Un autre article',
-            content: 'Ceci est le contenu d\'un autre article.',
-            author: {
-                connect: { id: user2.id },
-            },
-        },
+    console.log('Commentaires créés');
+
+
+    const likes = await prisma.like.createMany({
+        data: [
+            { userId: userList[0].id, articleId: articleList[1].id },
+            { userId: userList[1].id, articleId: articleList[2].id },
+            { userId: userList[2].id, articleId: articleList[3].id },
+            { userId: userList[3].id, articleId: articleList[4].id },
+            { userId: userList[4].id, articleId: articleList[0].id },
+        ],
     });
 
-    console.log('Articles créés:', article1, article2);
+    console.log('Likes créés');
 
-    const comment1 = await prisma.comment.create({
-        data: {
-            content: 'Super article !',
-            author: {
-                connect: { id: user2.id },
-            },
-            article: {
-                connect: { id: article1.id },
-            },
-        },
-    });
-
-    const comment2 = await prisma.comment.create({
-        data: {
-            content: 'Merci pour cet article informatif.',
-            author: {
-                connect: { id: user1.id },
-            },
-            article: {
-                connect: { id: article2.id },
-            },
-        },
-    });
-
-    console.log('Commentaires créés:', comment1, comment2);
-
-
-    const like1 = await prisma.like.create({
-        data: {
-            user: {
-                connect: { id: user1.id },
-            },
-            article: {
-                connect: { id: article2.id },
-            },
-        },
-    });
-
-    const like2 = await prisma.like.create({
-        data: {
-            user: {
-                connect: { id: user2.id },
-            },
-            article: {
-                connect: { id: article1.id },
-            },
-        },
-    });
-
-    console.log('Likes créés:', like1, like2);
+    console.log('📦 Seed terminé avec succès !');
 }
 
 seed()
     .catch(e => {
-        console.error(e);
+        console.error('Erreur lors du seed :', e);
         process.exit(1);
     })
     .finally(async () => {
